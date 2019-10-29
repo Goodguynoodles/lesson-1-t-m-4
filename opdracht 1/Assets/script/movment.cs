325bd7ffcfe2b964d27ce3f03a3f1d0f695d467a
koos van losenoord <316595@student.mboutrecht.nl>
2019-10-29 11:05:30 +0100

    no message

---
+++ opdracht 1/Assets/script/movment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment : MonoBehaviour
+{
+    public float speed = 100.0f;
+    public float turnspeed = 45.0f;
+    private float forward;
+    private float horizontalInput;
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        forward = Input.GetAxisRaw("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forward);
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+    }
+}
